ea9491e063fdc7479e1ff185f1091937150ffc26
bellrise <bellrise.dev@gmail.com>
2023-07-10 18:47:44 +0200

    feat(debug): add node_dump function

--- src/debug.c
+++ src/debug.c
@@ -53,25 +53,65 @@ void token_list_dump(struct token_list *tokens)
         const char *name;
     };
 
-#define TN(TOK)                                                                \
+#define TK(TOK)                                                                \
  {                                                                             \
-  TK_##TOK, #TOK                                                               \
+  .kind = TK_##TOK, .name = #TOK                                               \
  }
 
-    static const struct token_name token_names[] = {TN(EOF), TN(SYMBOL),
-                                                    TN(STRING)};
+    static const struct token_name token_names[] = {TK(EOF), TK(SYMBOL),
+                                                    TK(STRING)};
+
+#undef TK
 
     struct token *tok;
 
-    for (size_t i = 0; i < tokens->len; i++) {
+    for (int i = 0; i < tokens->len; i++) {
         tok = &tokens->tokens[i];
 
         for (size_t j = 0; j < TABLE_SIZE(token_names); j++) {
             if (tok->kind == token_names[j].kind) {
-                printf("%s len=%d pos=`%.*s`\n", token_names[j].name, tok->len,
-                       tok->len, tok->pos);
+                printf("\033[1;39m%s\033[0m len=%d pos=`%.*s`\n",
+                       token_names[j].name, tok->len, tok->len, tok->pos);
                 break;
             }
         }
     }
+}
+
+static void node_dump_level(struct node *node, int level)
+{
+#define FIELD(NOD) [NOD_##NOD] = #NOD,
+    static const char *node_names[] = {FIELD(MODULE)};
+#undef FIELD
+
+    for (int i = 0; i < level; i++)
+        fputs("  ", stdout);
+
+    /* Print this */
+
+    printf("\033[1;39m%s\033[0m", node_names[node->kind]);
+
+    switch (node->kind) {
+    case NOD_MODULE:
+        printf(" source_path=`%s`", NA_MODULE(node)->source_path);
+        break;
+    case NOD_FUNCTION:
+        printf(" name=`%s`", NA_FUNCTION(node)->name);
+        break;
+    }
+
+    fputc('\n', stdout);
+
+    /* Print the children. */
+    if (node->child)
+        node_dump_level(node->child, level + 1);
+
+    /* Print the next one. */
+    if (node->next)
+        node_dump_level(node->next, level);
+}
+
+void node_dump(struct node *node)
+{
+    node_dump_level(node, 0);
 }
